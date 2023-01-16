81f322bfe6ee70909934beb1f0b6a21139c18ea7
Garima Langer <105547526+GarimaLanger@users.noreply.github.com>
2023-01-16 13:03:37 +0530

    Add files via upload

---
+++ ADANI ENTERPRISE.py
@@ -0,0 +1,13 @@
+import numpy as np
+import random
+
+def c2dlist(numberOfRows,numberOfColumns):
+    l=np.random.randint(0, 100, size=(numberOfRows,numberOfColumns))
+    
+def main():
+    nr=int(input("Enter the number of rows: "))
+    nc=int(input("Enter the number of columns: "))
+    c2dlist(nr,nc)
+    
+if name == "main":
+    main()
