83f7a332370dd91d61aeebd56ae03f6386e75b1a
Garima Langer <105547526+GarimaLanger@users.noreply.github.com>
2023-01-16 13:08:47 +0530

    Delete Adani Test.py

--- Adani Test.py
+++
@@ -1,7 +0,0 @@
-import random
-def matrix_random(r, c):
-    return [[random.randrange(0, 101) for i in range(c)] for j in range(r)]
-
-r = int(input("Enter rows"))
-c = int(input("Enter columns"))
-print(matrix_random(r, c))
